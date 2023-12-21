ee7f363d4bf6ff4b4f784e8788df713973ebaba8
Zynkah <ffgurl07@yahoo.com>
2023-12-21 14:06:26 -0600

    Feat: Navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import { ModeToggle } from "@/components/theme-button";
-
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="sticky top-2 right-2"><ModeToggle /></div>
-       
       </div>
     </main>
   )
---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { ModeToggle } from "./theme-button";
+import Link from "next/link";
+
+
+export default function Navbar() {
+  return (
+    <div className="fixed w-full bg-slate-600 p-2">
+      <NavigationMenu className="m-6 gap-16 text-lg">
+        <NavigationMenuList className="hover:scale-110 hover:text-bold text-white">
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink>Home</NavigationMenuLink>
+          </Link>
+        </NavigationMenuList>
+
+        <NavigationMenuList>
+          <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
+            <Link href="/articles" legacyBehavior passHref>
+              <NavigationMenuLink>Articles</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+
+        <NavigationMenuList>
+          <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
+            <Link href="/projects" legacyBehavior passHref>
+              <NavigationMenuLink>Projects</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+
+        <NavigationMenuList>
+          <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
+            <Link href="/contact" legacyBehavior passHref>
+              <NavigationMenuLink>Contact</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+
+        <NavigationMenuList className="fixed top-6 right-1">
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <ModeToggle />
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
