f437dae875cc95c1d0928b18f06d1df88ba5fb04
Zynkah <ffgurl07@yahoo.com>
2023-12-22 11:54:42 -0600

    Feat: Accordian added to each article card

--- app/articles/ArticleCards.tsx
+++ app/articles/ArticleCards.tsx
@@ -1,3 +1,5 @@
+import { ArticleData } from "@/data/ArticleData";
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -6,24 +8,44 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ArticleData } from "@/data/ArticleData";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Separator } from "@/components/ui/separator";
 
 export default function ArticleCards() {
-    return (
-        <div>
+  return (
+    <div>
       {ArticleData.article.map((article, index) => (
-      <Card key={index} className="m-6">
-        <CardHeader>
-          <CardTitle>{article.title}</CardTitle>
-          <CardDescription>{article.description}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>{article.content}</p>
-        </CardContent>
-        <CardFooter>
-          <p>{article.footer}</p>
-        </CardFooter>
-      </Card>))}
-        </div>
-    );
+        <Card key={index} className="m-10">
+          <CardHeader>
+            <CardTitle className="text-3xl">{article.title}</CardTitle>
+            <Separator />
+          </CardHeader>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1" className="mx-6">
+              <AccordionTrigger>
+                <CardDescription className="text-lg text-slate-400 hover:scale-110">
+                  {article.description}
+                </CardDescription>
+              </AccordionTrigger>
+              <AccordionContent>
+                <CardContent>
+                  <p>{article.content}</p>
+                </CardContent>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <CardFooter>
+            <Link href="" className="mt-4 text-xs text-slate-400 hover:scale-110">
+              {article.footer}
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
