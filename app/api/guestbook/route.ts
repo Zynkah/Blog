7015e1423865289f306b69cb3f4a6ac2c211c720
Zynkah <ffgurl07@yahoo.com>
2023-12-23 23:58:08 -0600

    Feat: Drizzle started

--- app/api/guestbook/route.ts
+++ app/api/guestbook/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse, NextRequest } from "next/server";
-import { db } from '@/db';
-
+// import { NextResponse, NextRequest } from "next/server";
+// import { db } from '@/db';
+// import nanoid from 'nanoid';
 
 // export async function POST(req: NextRequest) {
 //   try {
@@ -8,16 +8,24 @@ import { db } from '@/db';
 
 
 //     const newGuestbookObject = {
-
+//       id: nanoid()
+//       name: 
+//       email:
+//       password:
+//       role:
+//       createdAt:
+//       updatedAt:
 //     }
 
 
-    // const createGuestbook = await db
-    //   .insert()
-    //   .values()
-    //   .returning();
+//     const createGuestbook = await db
+//       .insert()
+//       .values()
+//       .returning();
 
 //   } catch (error:any) {
 //     return NextResponse.error();
 //   }
 // }
+
+
