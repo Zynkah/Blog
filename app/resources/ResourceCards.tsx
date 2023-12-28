74290adac12181d1eebed54868b3662bf4999130
Zynkah <ffgurl07@yahoo.com>
2023-12-27 23:22:27 -0600

    Feat: Resources data added, links added

--- app/resources/ResourceCards.tsx
+++ app/resources/ResourceCards.tsx
@@ -5,7 +5,15 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { ResourceData } from "@/data/ResourceData";
+import Link from "next/link";
+
 
 export default function ResourceCards() {
   return (
@@ -14,7 +22,7 @@ export default function ResourceCards() {
         <ResizablePanelGroup
           key={index}
           direction="vertical"
-          className="min-h-[400px] min-w-[400px] rounded-lg border"
+          className="min-h-[500px] min-w-[400px] rounded-lg border"
         >
           <ResizablePanel defaultSize={25}>
             <div className="flex h-full items-center justify-center p-6">
@@ -23,8 +31,60 @@ export default function ResourceCards() {
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">{resource.content}</span>
+            <div className="flex h-full p-6">
+              <ul>
+                <p className="font-semibold mb-2">{resource.content}</p>
+                <p className="mb-2">
+                  Enjoy these links to get you started on your learning:
+                </p>
+                <li>
+                  <HoverCard>
+                    <HoverCardTrigger asChild>
+                      <Link
+                        href={resource.docs}
+                        target="blank"
+                        className="hover:underline decoration-sky-500"
+                      >
+                        ♡ Documentation
+                      </Link>
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-80">
+                      <div className="flex justify-between space-x-4">
+                        <Avatar>
+                          <AvatarImage src={resource.image} />
+                          <AvatarFallback>N/A</AvatarFallback>
+                        </Avatar>
+                        <div className="space-y-1">
+                          <h4 className="text-sm font-semibold">
+                            {resource.header}
+                          </h4>
+                          <p className="text-sm">
+                            Click here to be taken to the official documentation.
+                          </p>
+                        </div>
+                      </div>
+                    </HoverCardContent>
+                  </HoverCard>
+                </li>
+                <li>
+                  <Link
+                    href={resource.tutorial}
+                    target="blank"
+                    className="hover:underline decoration-sky-500"
+                  >
+                    ♡ Tutorial
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={resource.course}
+                    target="blank"
+                    className="hover:underline decoration-sky-500"
+                  >
+                    ♡ Course
+                  </Link>
+                </li>
+              </ul>
             </div>
           </ResizablePanel>
         </ResizablePanelGroup>
--- data/ResourceData.ts
+++ data/ResourceData.ts
@@ -1,26 +1,62 @@
 export const ResourceData = [
   {
-    header: "header 1",
-    content: "content",
+    header: "Typescript",
+    content:
+      "TypeScript adds additional syntax to JavaScript to support a tighter integration with your editor. Catch errors early in your editor. TypeScript code converts to JavaScript, which runs anywhere JavaScript runs: In a browser, on Node.js or Deno and in your apps. TypeScript understands JavaScript and uses type inference to give you great tooling without additional code.",
+    docs: "https://www.typescriptlang.org/",
+    tutorial: "https://www.w3schools.com/typescript/index.php",
+    course:
+      "https://www.codecademy.com/learn/learn-typescript?utm_source=pepperjam&utm_medium=affiliate&utm_term=159404&clickId=4612857686&pj_creativeid=2-438167&pj_publisherid=159404",
+    image: "/img/Typescript.png",
   },
   {
-    header: "header 2",
-    content: "content",
+    header: "React.js",
+    content:
+      "React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.",
+    docs: "https://react.dev/",
+    tutorial: "https://www.w3schools.com/react/default.asp",
+    course:
+      "https://www.codecademy.com/learn/react-101?utm_source=pepperjam&utm_medium=affiliate&utm_term=159404&clickId=4612859371&pj_creativeid=2-438167&pj_publisherid=159404",
+    image: "/img/React.png",
   },
   {
-    header: "header 3",
-    content: "content",
+    header: "Next.js",
+    content:
+      "Used by some of the world's largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+    docs: "https://nextjs.org/",
+    tutorial:
+      "https://dev.to/joshhortt/a-beginners-guide-to-building-your-first-nextjs-application-step-by-step-tutorial-glf",
+    course: "https://nextjs.org/learn",
+    image: "/img/Next.png",
   },
   {
-    header: "header 4",
-    content: "content",
+    header: "Tailwind CSS",
+    content:
+      "Rapidly build modern websites without ever leaving your HTML. A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.",
+    docs: "https://tailwindcss.com/",
+    tutorial: "https://www.geeksforgeeks.org/tailwind-css/",
+    course: "https://v1.tailwindcss.com/course",
+    image: "/img/Tailwind.webp",
   },
   {
-    header: "header 5",
-    content: "content",
+    header: "Drizzle ORM",
+    content:
+      "Drizzle has always been fast, we just wanted you to have proper benchmarks experience. Well, actually it's not that Drizzle is fast, Drizzle just doesn't slow you down",
+    docs: "https://orm.drizzle.team/",
+    tutorial:
+      "https://blog.logrocket.com/getting-started-drizzle-type-safe-orm/",
+    course:
+      "https://www.youtube.com/playlist?list=PLhnVDNT5zYN8PLdYddaU3jiZXeOyehhoU",
+    image: "/img/Drizzle.jpg",
   },
   {
-    header: "header 6",
-    content: "content",
+    header: "Next-Auth",
+    content:
+      "Built in support for popular services (Google, Facebook, Auth0, Apple…). Use with OAuth 1.0 & 2.0 services. Built for Serverless, runs anywhere. Bring Your Own Database - or none! Choose database sessions or JWT. Secure web pages and API routes. Signed, prefixed, server-only cookies. HTTP POST + CSRF Token validation. JWT with JWS / JWE / JWK. Tab syncing, auto-revalidation, keepalives. Doesn't rely on client side JavaScript",
+    docs: "https://next-auth.js.org/",
+    tutorial:
+      "https://dev.to/alishirani/step-by-step-tutorial-on-how-to-use-next-auth-in-nextjs-13-using-route-handlers-2jmc",
+    course: "https://www.youtube.com/watch?v=t0Fs0NO78X8",
+    image: "/img/NextAuth.png",
   },
 ];
