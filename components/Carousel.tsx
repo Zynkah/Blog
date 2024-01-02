84f0732bbab7cc880f0b0b8e356117269965f585
Zynkah <ffgurl07@yahoo.com>
2024-01-02 14:15:20 -0600

    Feat: active and inactive cards styles on carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -10,8 +10,22 @@ import {
 import { CarouselData } from "@/data/CarouselData";
 import { Card, CardContent } from "./ui/card";
 import Link from "next/link";
+import { useState } from "react";
+
 
 export default function HomeCarousel() {
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % CarouselData.length);
+  };
+
+  const handlePreviousClick = () => {
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + CarouselData.length) % CarouselData.length
+    );
+  };
+
   return (
     <div className="w-full max-w-5xl m-auto">
       <Carousel
@@ -21,30 +35,43 @@ export default function HomeCarousel() {
         }}
       >
         <CarouselContent className="-ml-1">
-          {Array.from({ length: 4 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/3 text-center"
-            >
-              <Link href={CarouselData[index]?.link}>
-                <div className="p-1">
-                  <Card className="hover:underline decoration-sky-500">
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <div className="h-[300px]">
-                        <h2 className="text-lg">
-                          {CarouselData[index]?.title}
-                        </h2>
-                        <p>{CarouselData[index]?.description}</p>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </div>
-              </Link>
-            </CarouselItem>
-          ))}
+          {Array.from({ length: 4 }).map((_, index) => {
+            const isActive = index === activeIndex;
+            return (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3 text-center"
+              >
+                <Link href={CarouselData[index]?.link}>
+                  <div className="p-1">
+                    <Card
+                      className={`hover:underline decoration-sky-500 ${
+                        isActive
+                          ? "scale-100 bg-slate-700"
+                          : "scale-90 blur-sm hover:blur-none"
+                      }`}
+                    >
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <div className="h-[300px]">
+                          <h2 className="text-lg">
+                            {CarouselData[index]?.title}
+                          </h2>
+                          <p>{CarouselData[index]?.description}</p>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </Link>
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div onClick={handlePreviousClick}>
+          <CarouselPrevious />
+        </div>
+        <div onClick={handleNextClick}>
+          <CarouselNext />
+        </div>
       </Carousel>
     </div>
   );
--- data/CarouselData.ts
+++ data/CarouselData.ts
@@ -1,20 +1,24 @@
 export const CarouselData = [
   {
+    id: 1,
     title: "Read My Articles",
     description: "description",
     link: "/articles",
   },
   {
+    id: 2,
     title: "Find Free Resources",
     description: "description",
     link: "/resources",
   },
   {
+    id: 3,
     title: "View My Projects",
     description: "description",
     link: "/projects",
   },
   {
+    id: 4,
     title: "Contact Me",
     description: "description",
     link: "/contact",
