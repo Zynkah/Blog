bd037520e9dbb7d0d73bad8aabfed0d61c7c9a49
Zynkah <ffgurl07@yahoo.com>
2024-01-06 17:49:04 -0600

    Feat: Hamburger added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Menu } from "lucide-react";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -5,57 +10,61 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { ModeToggle } from "./theme-button";
-import Link from "next/link";
-
+import { Button } from "./ui/button";
 
 export default function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <div className="sticky top-0 w-full bg-slate-600 p-2">
-      <NavigationMenu className="m-6 md:gap-16 gap-4 text-lg">
-        <NavigationMenuList className="hover:scale-110 hover:text-bold text-white">
-          <Link href="/" legacyBehavior passHref>
-            <NavigationMenuLink>Home</NavigationMenuLink>
-          </Link>
-        </NavigationMenuList>
+      <Button
+        className="bg-slate-600 hover:bg-slate-600 text-white sm:hidden"
+        onClick={() => setIsNavOpen(!isNavOpen)}
+      >
+        <Menu />
+      </Button>
 
-        <NavigationMenuList>
+      <NavigationMenu
+        className={`mobile-nav-content ${isNavOpen ? "open" : ""}`}
+      >
+        <NavigationMenuList className="mobile-menu-list m-6 md:gap-16 gap-4 text-lg">
+          <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink>Home</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
           <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
             <Link href="/articles" legacyBehavior passHref>
               <NavigationMenuLink>Articles</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-        </NavigationMenuList>
 
-        <NavigationMenuList>
           <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
             <Link href="/resources" legacyBehavior passHref>
               <NavigationMenuLink>Resources</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-        </NavigationMenuList>
 
-        <NavigationMenuList>
           <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
             <Link href="/projects" legacyBehavior passHref>
               <NavigationMenuLink>Projects</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-        </NavigationMenuList>
 
-        <NavigationMenuList>
           <NavigationMenuItem className="hover:scale-110 hover:text-bold text-white">
             <Link href="/contact" legacyBehavior passHref>
               <NavigationMenuLink>Contact</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-        </NavigationMenuList>
 
-        <NavigationMenuList className="fixed top-6 right-1">
-          <NavigationMenuItem>
-            <NavigationMenuLink>
-              <ModeToggle />
-            </NavigationMenuLink>
-          </NavigationMenuItem>
+          <NavigationMenuList className="fixed md:top-6 top-2 right-1">
+            <NavigationMenuItem>
+              <NavigationMenuLink>
+                <ModeToggle />
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
