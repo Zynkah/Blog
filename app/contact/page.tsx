be1ca498db767359b77ba30fe157625b2d6c2d70
Zynkah <ffgurl07@yahoo.com>
2024-01-17 17:59:32 -0600

    Fix: light mode colors and carousel fixed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 export default function Contact() {
   return (
     <div className="p-2">
-      <div className="border w-max px-5 py-2 m-auto my-5 rounded hover:bg-slate-600 hover:scale-110">
+      <div className="border w-max px-5 py-2 m-auto my-5 rounded hover:bg-slate-300 hover:dark:bg-slate-600  border-slate-400 dark:border-slate-600 hover:scale-110">
         <Dialog>
           <DialogTrigger>View My Contact Card</DialogTrigger>
           <DialogContent>
--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -40,20 +40,20 @@ export default function HomeCarousel() {
             return (
               <CarouselItem
                 key={index}
-                className="pl-1 md:basis-1/2 lg:basis-1/3 text-center"
+                className={`pl-1 md:basis-1/3 text-center ${isActive ? 'z-10' : 'z-0'} `}
               >
                 <Link href={CarouselData[index]?.link}>
                   <div className="p-1">
                     <Card
-                      className={`hover:underline decoration-sky-500 ${
+                      className={`hover:bg-slate-300 hover:dark:bg-slate-600  border-slate-400 dark:border-slate-600 ${
                         isActive
-                          ? "bg-slate-700"
-                          : "sm:scale-90 sm:blur-sm hover:blur-none"
+                          ? "md:w-[400px] lg:w-[500px] xl:w-[600px] md:-ml-28"
+                          : "md:scale-90 xl:scale-100 lg:mt-8 xl:mt-20 md:blur-sm hover:blur-none"
                       }`}
                     >
                       <CardContent className="flex aspect-square items-center justify-center p-6">
                         <div className="h-[400px]">
-                          <h2 className="text-lg">
+                          <h2 className="text-xl">
                             {CarouselData[index]?.title}
                           </h2>
                           <p>{CarouselData[index]?.description}</p>
