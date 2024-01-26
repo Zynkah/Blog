57fd7ea624bad85fce83fa42aa5ce9d0ba360ef7
Zynkah <ffgurl07@yahoo.com>
2024-01-26 11:46:12 -0600

    Fix: Security issues

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,4 +1,5 @@
-"use client";
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { FormEvent } from "react";
 import { signIn } from "next-auth/react";
@@ -6,18 +7,34 @@ import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
   const router = useRouter();
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     const formData = new FormData(e.currentTarget);
-    const resp = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false,
-    });
-    console.log({ resp });
-    if (!resp?.error) {
-      router.push("/");
-      router.refresh();
+    const email = formData.get("email")?.toString().trim();
+    const password = formData.get("password")?.toString().trim();
+
+    if (!email || !password) {
+      console.error("Email and password are required.");
+      return;
+    }
+
+    try {
+      const resp = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (!resp?.error) {
+        router.push("/");
+        router.refresh();
+      } else {
+        console.error("Authentication failed:", resp.error);
+      }
+    } catch (error) {
+      console.error("An unexpected error occurred:", error);
     }
   };
 
@@ -30,13 +47,15 @@ export default function LoginForm() {
         name="email"
         className="border rounded"
         type="email"
-        placeholder="email"
+        placeholder="Email"
+        required
       />
       <input
         name="password"
         className="border rounded"
         type="password"
-        placeholder="password"
+        placeholder="Password"
+        required
       />
       <Button className="m-auto w-24" type="submit">
         Login
