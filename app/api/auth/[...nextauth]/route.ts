2f2b07919cfffdeaf9942a5f863d1e399bc8afde
Zynkah <ffgurl07@yahoo.com>
2024-01-26 12:10:04 -0600

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,50 @@
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { compare } from "bcrypt";
-// import { user } from "@/db/schema";
-// import { db } from "@/db";
-// import { eq } from "drizzle-orm";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+import { user } from "@/db/schema";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
 
-// const handler = NextAuth({
-//   session: {
-//     strategy: "jwt",
-//   },
-//   pages: {
-//     signIn: "/login",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         email: {},
-//         password: {},
-//       },
+const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
 
-//       async authorize(credentials) {
-//         const response = await db
-//           .select()
-//           .from(user)
-//           .where(eq(user.email, credentials?.email || ""));
+      async authorize(credentials) {
+        const response = await db
+          .select()
+          .from(user)
+          .where(eq(user.email, credentials?.email || ""));
 
-//         const users = response[0];
+        const users = response[0];
 
-//         const passwordCorrect = await compare(
-//           credentials?.password || "",
-//           users.password || ""
-//         );
+        const passwordCorrect = await compare(
+          credentials?.password || "",
+          users.password || ""
+        );
 
-//         if (passwordCorrect) {
-//           return {
-//             id: users.id,
-//             email: users.email,
-//           };
-//         }
+        if (passwordCorrect) {
+          return {
+            id: users.id,
+            email: users.email,
+          };
+        }
 
-//         return null;
-//       },
-//     }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
